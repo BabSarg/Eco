27b41d84915f0ab0db01bd23a5f625198566dff4
Babken_Sargsyan <babsargs@gamil.com>
2022-11-29 10:51:15 +0400

    add filter for ecoService and waste

--- Eco/src/main/java/eco_service/Eco/repositories/EcoServiceRepository.java
+++ Eco/src/main/java/eco_service/Eco/repositories/EcoServiceRepository.java
@@ -2,13 +2,13 @@ package eco_service.Eco.repositories;
 
 import eco_service.Eco.models.EcoService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface EcoServiceRepository extends JpaRepository<EcoService, Long> {
+public interface EcoServiceRepository extends JpaRepository<EcoService, Long>, QuerydslPredicateExecutor<EcoService> {
 
     EcoService findByName(String name);
 
--- Eco/src/main/java/eco_service/Eco/services/EcoServiceService.java
+++ Eco/src/main/java/eco_service/Eco/services/EcoServiceService.java
@@ -4,13 +4,14 @@ import eco_service.Eco.dtos.ChangePasswordEcoServiceDto;
 import eco_service.Eco.dtos.EcoServiceAddDTO;
 import eco_service.Eco.dtos.EcoServiceDTO;
 import eco_service.Eco.exceptions.ErrorResponse;
+import eco_service.Eco.filter.EcoServiceFilter;
 import eco_service.Eco.response.Response;
 
 import java.util.List;
 
 public interface EcoServiceService {
 
-    Response<ErrorResponse, List<EcoServiceDTO>> getAll();
+    Response<ErrorResponse, List<EcoServiceDTO>> getAll(EcoServiceFilter ecoServiceFilter);
 
     Response<ErrorResponse, EcoServiceDTO> getById(Long id);
 
