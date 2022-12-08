9c32c56517a7dba13aeb178014a83192ee3ebf37
Babken_Sargsyan <babsargs@gamil.com>
2022-12-08 23:05:05 +0400

    Revert "entity"
    This reverts commit 6b45ba18ca18137d71b7aee1b227aa623a0ed6bd.

--- Eco/src/main/java/eco_service/Eco/models/Order.java
+++ Eco/src/main/java/eco_service/Eco/models/Order.java
@@ -19,6 +19,9 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
+    @Column(name = "eco_service_id")
+    private Long ecoServiceId;
+
     @Column(name = "waste_id")
     private Long wasteId;
 
@@ -27,9 +30,6 @@ public class Order {
 
     @Column(name = "customer_email")
     private String customerEmail;
-
-    @Column(name = "customer_phone")
-    private String customerPhone;
 
     @Column(name = "description")
     private String description;
